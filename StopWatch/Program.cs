cb65e93bef918e430bfe783c49546c4ee50020fd
Marcelo Alves <marcelloalves09@gmail.com>
2023-12-14 19:32:57 -0300

    First Commit

---
+++ StopWatch/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Start(30);
+
+static void Start(int time)
+{
+    int currentTime = 0;
+    while (currentTime != time)
+    {
+        Console.Clear();
+        currentTime++;
+        Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+    }
+}
