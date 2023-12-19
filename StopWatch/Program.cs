619b84d41a901766794ae41f195e7a09bef94a12
Marcelo Alves <marcelloalves09@gmail.com>
2023-12-19 17:46:12 -0300

    Implementando novas funções

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -1,8 +1,45 @@
 ﻿
-Start(30);
+Menu();
 
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("WELCOME:");
+    Console.WriteLine(" S = Seconds => 10s = 10Seconds");
+    Console.WriteLine(" M = Minutes => 1m = 1 Minutes");
+    Console.WriteLine(" 0 = To go out");
+    Console.WriteLine("How long do you want to count?");
+
+    string data = Console.ReadLine().ToLower();
+    string type = data.Substring(data.Length - 1);
+    int time = int.Parse(data.Substring(0, data.Length - 1));
+    if(time == 0)
+        System.Environment.Exit(0);
+
+    if (type == "m")
+        PreStart(time * 60);
+    else
+    {
+        PreStart(time);
+    }
+ }
+
+static void PreStart (int time)
+{
+    Console.Clear();
+    Console.WriteLine("Ready.....");
+    Thread.Sleep(1000);
+    Console.WriteLine("Set.....");
+    Thread.Sleep(1000);
+    Console.WriteLine("Go.....");
+    Thread.Sleep(1000);
+
+    Start(time);
+    Menu();
+}
 static void Start(int time)
 {
+
     int currentTime = 0;
     while (currentTime != time)
     {
@@ -11,4 +48,7 @@ static void Start(int time)
         Console.WriteLine(currentTime);
         Thread.Sleep(1000);
     }
+    Console.Clear();
+    Console.WriteLine("Stop Stopwatch");
+    Thread.Sleep(1000);
 }
